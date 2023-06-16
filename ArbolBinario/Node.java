2b4e95660efda145b93a97e4f5fd3f0a804704a0
Darwin Andrés Sarango Soto <132624867+DarwinSarango@users.noreply.github.com>
2023-06-15 20:52:43 -0500

    Update Node.java

--- ArbolBinario/Node.java
+++ ArbolBinario/Node.java
@@ -4,14 +4,15 @@ public int getDato() {   //se crea el get para acceder a los datos
 public void setDato(int dato) { // se crea el set para acceder a los datos
         this.dato = dato;  // se le asigna el dato a la clase
     }
-//se crea el get y el set para poder acceder a los datos de la clase
+//se crea el get y el set de Node para acceder a los datos de la clase
 public Node getIzquierdo() {    
         return izquierdo; 
     }
 public void setIzquierdo(Node izquierdo) { 
        this.izquierdo = izquierdo; 
     }
- public Node getDerecho() {  // se crea el get y el set para poder acceder a los datos de la clase
+ // se crea el get y el set para poder acceder a los datos de la clase
+ public Node getDerecho() {  
         return derecho; // se retorna el dato
     }
   public void setDerecho(Node derecho) {  
