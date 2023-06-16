539e7e5deefa9903ef9ff3c8913f452f1b997b23
Brian Fernando Aguinsaca Loarte <133794609+BrianFernandoAguinsacaLoarte@users.noreply.github.com>
2023-06-15 21:04:22 -0500

    Update Serie.java

--- ArbolBinario/Serie.java
+++ ArbolBinario/Serie.java
@@ -1,4 +1,7 @@
 
 public class Serie {   //Creación de la clase Serie 
+  
   private int valor=0;  // Se crea un atributo de valor de tipo privado que tiene como valor 0
+  private int valor2=1; // Se crea un atributo de valor2 de tipo privado que tiene como valor 1
+  
 }
