e7066799c5117e68fa8deed66e5f2ee5422dc4c6
Darwin Andrés Sarango Soto <132624867+DarwinSarango@users.noreply.github.com>
2023-06-15 20:44:07 -0500

    Update Node.java

--- ArbolBinario/Node.java
+++ ArbolBinario/Node.java
@@ -1,6 +1,9 @@
-  public int getDato() {  
+public int getDato() {  
         return dato; 
     }
- public void setDato(int dato) { 
+public void setDato(int dato) { 
         this.dato = dato;
+    }
+public Node getIzquierdo() { 
+        return izquierdo; 
     }
