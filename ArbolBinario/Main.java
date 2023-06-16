0048e3023acac75751fb4fae2a911857b311f39d
Brian Fernando Aguinsaca Loarte <133794609+BrianFernandoAguinsacaLoarte@users.noreply.github.com>
2023-06-15 20:45:23 -0500

    Update Main.java

--- ArbolBinario/Main.java
+++ ArbolBinario/Main.java
@@ -1,5 +1,7 @@
 
 public class Main {
     public static void main(String[] args) { //Creación de la clase Main
+
+          Arbol arbol = new Arbol (5) //Creacion del objeto Arbol que inicializa con un valor de 5
         
     }
