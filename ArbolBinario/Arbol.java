280bc2eaf01336eca7507220781eaa1ee5c90e7a
Abel214 <131829158+Abel214@users.noreply.github.com>
2023-06-15 20:46:57 -0500

    Update Arbol.java
    En cada iteración del bucle:
    a. Actualiza la referencia padre con la referencia aux.
    b. Compara el valor del nodo actual aux.dato con el valor del dato que se va a agregar.
    c. Si el valor del dato a agregar es menor que el valor del nodo actual (dato<aux.dato), mueve la referencia aux hacia el nodo izquierdo (aux = aux.izquierdo).
    Si aux es null, significa que se ha encontrado un lugar vacío para insertar el nuevo nodo. En este caso, establece el hijo izquierdo del padre como el nuevo nodo: padre.izquierdo = nuevo;.

--- ArbolBinario/Arbol.java
+++ ArbolBinario/Arbol.java
@@ -1,7 +1,27 @@
 public class Arbol {
     public Node raiz;
     //constructor
-    public Arbol(int dato){
-        this.raiz=new Node(dato); 
-
+    public Arbol(int dato){  //Crea un nuevo nodo con el valor proporcionado: Node nuevo = new Node(dato);.
+        this.raiz=new Node(dato);  //Crea una referencia auxiliar aux y la inicializa con la raíz del árbol: Node aux = raiz;.
+        public void agregarDato(int dato){
+        Node nuevo = new Node(dato);
+        Node aux = raiz; //Crea una referencia padre que inicialmente apunta a null.
+        Node padre;
+        while (true){
+            padre = aux;
+            if (dato<aux.dato){
+                aux = aux.izquierdo;
+                if (aux == null){
+                    padre.izquierdo = nuevo;
+                    return;
+                }
+            }else{
+                aux = aux.derecho;
+                if (aux == null){
+                    padre.derecho = nuevo;
+                    return;
+                }
+            }
+        }
+    }
     }
