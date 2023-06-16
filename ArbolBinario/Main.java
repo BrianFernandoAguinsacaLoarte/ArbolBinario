2017b7366bb7408496088512c62c9ec4c2ce4804
Brian Fernando Aguinsaca Loarte <133794609+BrianFernandoAguinsacaLoarte@users.noreply.github.com>
2023-06-15 20:52:13 -0500

    Update Main.java

--- ArbolBinario/Main.java
+++ ArbolBinario/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) { //Creación de la clase Main
 
           Arbol arbol = new Arbol (5) //Creacion del objeto Arbol que inicializa con un valor de 5
-          arbol.raiz.izquierdo =new Node (3); // arbol llama a método raiz y el lado izquierda, el cual se presenta con valor de 3 al crear el objeto Node
+          arbol.raiz.izquierdo =new Node (3); // Se crea un objeto Node que tiene como valor 3, del atributo izquierdo, el objeto raiz y la instancia arbol
+         
         
     }
